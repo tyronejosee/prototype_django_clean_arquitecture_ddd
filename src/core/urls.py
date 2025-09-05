d61359482de1bf582636169d8eee70047b4a3796
Tyrone Jose <alt.tyronejose@gmail.com>
2025-09-05 12:57:27 -0400

    build: redirect root to docs and use .env in docker-compose

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from src.core.health_chech import health_check
 
 urlpatterns: list = [
-    path("", health_check),
+    path("", RedirectView.as_view(url="docs", permanent=False)),
+    path("health-check", health_check),
     path("admin/", admin.site.urls),
     path("docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
     path("redoc", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
