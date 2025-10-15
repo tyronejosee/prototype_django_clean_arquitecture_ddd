8d713d5ef583003227dff3ae0504caf0466fcde4
Tyrone Jose <alt.tyronejose@gmail.com>
2025-10-15 15:30:22 -0300

    style(admin): update admin panel theme colors and layout styles

--- src/core/urls.py
+++ src/core/urls.py
@@ -27,3 +27,8 @@ urlpatterns: list = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom attributes for admin
+admin.site.site_header = "Marketly"
+admin.site.site_title = "Marketly"
+admin.site.index_title = "Admin"
